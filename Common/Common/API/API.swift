55388ac665c62a92517f1947f1a3745ca9bf2fe4
Patrick Jiang <brojyf@163.com>
2025-09-08 22:11:07 -0700

    docs(API): implemented api for login section for frontend]

---
+++ Common/Common/API/API.swift
@@ -0,0 +1,31 @@
+//
+//  API.swift
+//  Common
+//
+//  Created by 江逸帆 on 9/8/25.
+//
+
+import Foundation
+
+enum API {
+    #if DEBUG
+    static let base = URL(string: "http://localhost:8080/api")!
+    #else
+    static let base = URL(string: "https://patjiang.dpdns.org/api")!
+    #endif
+    
+    enum Auth {
+        private static let authBase = base.appendingPathComponent("auth")
+                
+        static let requestCode   = authBase.appendingPathComponent("request-code")
+        static let verifyCode    = authBase.appendingPathComponent("verify-code")
+        static let createAccount = authBase.appendingPathComponent("create-account")
+        static let refresh       = authBase.appendingPathComponent("refresh")
+    }
+    
+    enum User {
+        private static let userBase = base.appendingPathComponent("user")
+        
+        static let setUsername  = userBase.appendingPathComponent("set-username")
+    }
+}
