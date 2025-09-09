00c2e84437ec7d489ba3532bb3dcc5cd440f99a9
Patrick Jiang <brojyf@163.com>
2025-09-08 19:48:23 -0700

    first commit

---
+++ Common/Common/CommonApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommonApp.swift
+//  Common
+//
+//  Created by 江逸帆 on 9/8/25.
+//
+
+import SwiftUI
+
+@main
+struct CommonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
