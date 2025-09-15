f458a29cf95347614d13bfc822ab84e85e898509
Patrick Jiang <brojyf@163.com>
2025-09-15 08:34:45 -0700

    imp(test.sh): Implemented parse json automatically

--- Backend/cmd/test/test.sh
+++ Backend/cmd/test/test.sh
@@ -1,15 +1,15 @@
 # Request Code
 printf "\033[32m*** /auth/request-code ***\033[0m\n"
-curl -i -X POST http://localhost:8080/api/auth/request-code \
+response=$(curl -si -X POST http://localhost:8080/api/auth/request-code \
   -H "Content-Type:application/json" \
-  -d '{"email":"patrick.jiang@plu.edu","scene":"signup"}'
+  -d '{"email":"patrick.jiang@plu.edu","scene":"signup"}')
+echo "$response"
 
 #Verify Code
 printf "\n\033[32m*** /auth/verify-code ***\033[0m\n"
 echo "Enter the code: "
 read code
-echo "Enter the code_id: "
-read codeID
+codeID=$(echo "$response" | jq -r '.otp_id')
 curl -i -X POST http://localhost:8080/api/auth/verify-code \
   -H "Content-Type:application/json" \
   -d "{\"otp_id\":\"$codeID\",\"code\":\"$code\",\"email\":\"patrick.jiang@plu.edu\",\"scene\":\"signup\"}"
