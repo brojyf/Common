51080986743555143b33a8650e65d6dbee6937c4
Patrick Jiang <brojyf@163.com>
2025-09-18 14:24:43 -0700

    imp(test.sh): Impelemented verify-code test

--- backend/cmd/test/test.sh
+++ backend/cmd/test/test.sh
@@ -12,22 +12,22 @@ codeID=$(jq -er '.otp_id' /tmp/req_body.json) || {
   echo "failed to parse .otp_id from body"; exit 1;
 }
 
-## Verify Code
-#printf "\n\033[32m*** /auth/verify-code ***\033[0m\n"
-#echo "Enter the code: "
-#read code
-#curl -s -D /tmp/req_hdr.txt -o /tmp/req_body.json \
-#  -X POST http://localhost:8080/api/auth/verify-code \
-#  -H "Content-Type:application/json" \
-#  -d "{\"otp_id\":\"$codeID\",\"code\":\"$code\",\"email\":\"patrick.jiang@plu.edu\",\"scene\":\"signup\"}"
-#
-#cat /tmp/req_hdr.txt
-#cat /tmp/req_body.json
-#
-#otp=$(jq -er '.token' /tmp/req_body.json) || {
-#  echo "failed to parse .token from body"; exit 1;
-#}
-#
+# Verify Code
+printf "\n\033[32m*** /auth/verify-code ***\033[0m\n"
+echo "Enter the code: "
+read code
+curl -s -D /tmp/req_hdr.txt -o /tmp/req_body.json \
+  -X POST http://localhost:8080/api/auth/verify-code \
+  -H "Content-Type:application/json" \
+  -d "{\"otp_id\":\"$codeID\",\"code\":\"$code\",\"email\":\"patrick.jiang@plu.edu\",\"scene\":\"signup\"}"
+
+cat /tmp/req_hdr.txt
+cat /tmp/req_body.json
+
+otp=$(jq -er '.token' /tmp/req_body.json) || {
+  echo "failed to parse .token from body"; exit 1;
+}
+
 ## Create Account
 #printf "\n\033[32m*** /auth/create-account ***\033[0m\n"
 #curl -s -D /tmp/req_hdr.txt -o /tmp/req_body.json \
