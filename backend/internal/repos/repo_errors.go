5776070297941cc74be16f39ec95edc8d71de921
Patrick Jiang <brojyf@163.com>
2025-09-23 16:26:08 -0700

    imp(svc err & repo err): Add a few errors

--- backend/internal/repos/repo_errors.go
+++ backend/internal/repos/repo_errors.go
@@ -3,16 +3,21 @@ package repos
 import "errors"
 
 var (
-	// ErrRateLimited 429
-	ErrRateLimited = errors.New("throttle")
-
 	// ErrOTPInvalid 401
 	ErrOTPInvalid = errors.New("invalid one-time password")
 	// ErrOTPExpired 401
 	ErrOTPExpired = errors.New("expired one-time password")
 
+	// ErrEmailAlreadyExists 409
+	ErrEmailAlreadyExists = errors.New("email already exists")
+
+	// ErrRateLimited 429
+	ErrRateLimited = errors.New("throttle")
+
 	// ErrRunScript 500: Error when running lua script
 	ErrRunScript = errors.New("run script error")
 	// ErrUnexpectedReply 500: Unexpected reply from lua
 	ErrUnexpectedReply = errors.New("unexpected response")
+	// E
+	ErrUnexpectedSQL = errors.New("unexpecteed error when running sql")
 )
--- backend/internal/services/svc_errors.go
+++ backend/internal/services/svc_errors.go
@@ -5,6 +5,7 @@ import "errors"
 var (
 	ErrBadRequest     = errors.New("bad request")
 	ErrUnauthorized   = errors.New("unauthorized")
+	ErrConflict		  = errors.New("conflict")
 	ErrTooManyRequest = errors.New("too many requests")
 	ErrInternalServer = errors.New("internal server error")
 
