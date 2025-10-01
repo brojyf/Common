4809adb2c6691ae8a63563df44baa2866304e5d6
Patrick Jiang <brojyf@163.com>
2025-10-01 09:59:24 -0700

    docs(api): Implemented get pfp photo endpoint

---
+++ Common/Common/Core/Bill/BillView.swift
@@ -0,0 +1,18 @@
+//
+//  BillView.swift
+//  Common
+//
+//  Created by 江逸帆 on 9/30/25.
+//
+
+import SwiftUI
+
+struct BillView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BillView()
+}
---
+++ Common/Common/Core/Friends/FriendsView.swift
@@ -0,0 +1,18 @@
+//
+//  FriendsView.swift
+//  Common
+//
+//  Created by 江逸帆 on 9/30/25.
+//
+
+import SwiftUI
+
+struct FriendsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FriendsView()
+}
